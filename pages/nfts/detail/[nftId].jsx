15b0484b60ab993f7332075783afea147ace03f5
aung <aung_k@kkventureglobal.com>
2023-01-21 13:14:47 +0700

    vercel update

--- pages/nfts/detail/[nftId].jsx
+++ pages/nfts/detail/[nftId].jsx
@@ -7,7 +7,7 @@ const Aung = () => {
     console.log(nftId)
     localStorage.setItem('nftId',nftId)
     if(nftId){
-      window.location.href = '/nfts/details'
+      window.location.href = '/nfts/detail'
     }
   },[])
   return(
